43fc5a4fd7a43701342e4a8ac26829580b9a2464
unknown <belal.bilal.dev@gmail.com>
2022-03-02 16:26:00 -0800

    created project with create react app and added files for homepage

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import HomePage from './homepage.component'
+import './App.css';
+
+function App() {
+  return (
+    <HomePage/>
+  );
+}
+
+export default App;
---
+++ src/homepage.component.jsx
@@ -0,0 +1,43 @@
+import './homepage.styles.scss';
+
+function HomePage() {
+  return (
+    <div className="homepage">
+        <div className="directory-menu">
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">HATS</h1>
+                    <div className="subtitle">SHOP NOW</div>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">JACKETS</h1>
+                    <div className="subtitle">SHOP NOW</div>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">SNEAKERS</h1>
+                    <div className="subtitle">SHOP NOW</div>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">WOMENS</h1>
+                    <div className="subtitle">SHOP NOW</div>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">MENS</h1>
+                    <div className="subtitle">SHOP NOW</div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+  );
+}
+
+export default HomePage;
